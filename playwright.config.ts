0a245ca870310cc87bf62accc2735f9fc930728f
kateryna-goliak <kateryna.goliak@gmail.com>
2026-09-21 17:03:26 +0300

    chore: enable retain-on-failure trace and only-on-failure screenshot

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,8 +20,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL || 'http://104.168.59.50/articles',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /* Collect trace and screenshot when test fails. See https://playwright.dev/docs/trace-viewer */
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
